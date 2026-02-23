442f8dab0126abd4a68f0045404e2d12c3e5f580
Cooper Thiel <coth4827@colorado.edu>
2026-02-23 16:22:58 -0500

    Updated gitignore and setup datadownloader to download to data folder (created if not present)

--- scripts/dataDownloader.py
+++ scripts/dataDownloader.py
@@ -1,10 +1,15 @@
 import yfinance as yf
 import pandas as pd
+from pathlib import Path
 
 
+# Download ticker data into pandas dataframe
+ticker = "AAPL"
+
+# Using yfinance, remember it has survivorship bias
 df = yf.download(
 
-    "AAPL",
+    ticker,
 
     start="2000-01-01",
 
@@ -15,6 +20,27 @@ df = yf.download(
 )
 
 
+# save to parquet
+# This just saves one stock to its own parquet file.
+# May change to a partitioned dataset instead, that way if I want to do a strategy that uses the entire S&P 500 for example I don't have to open and close 500+ files
+def save_individual_ticker_to_parquet(df, ticker, filename=ticker.lower() + "_data.parquet"):
+    #Getting path to data folder
+    currentDirectory = Path(__file__).resolve().parent
+    dataFolder = currentDirectory.parent / "data"   
+    dataFolder.mkdir(parents=True, exist_ok=True)
+    filename = dataFolder / filename
+    #create data folder if it doesn't exist (it won't upon download because the folder is gitignored)
 
-print(df.head())
-print(df.shape)
+
+    # Normally, the columns are multilevel. Price "row" contains the various price columns: open, close, etc.
+    # and "Ticker" holds the ticker, but we are using one ticker and saving the ticker name in the filename, thus, we can remove the ticker column
+    if isinstance(df.columns, pd.MultiIndex):
+        df.columns = df.columns.get_level_values(0)
+
+    df.columns = [c.replace(' ', '_') for c in df.columns] # Replaces the " " in all columns. Not super useful here since the only " " is in adj close but good to be safe
+
+    df.to_parquet(filename, engine="pyarrow", index=False)
+    print("Saved to " + str(filename))
+
+
+save_individual_ticker_to_parquet(df, ticker)
